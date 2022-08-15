f2bd4136a0d78d69176fe3aa4729a4c12542f371
tapok39 <109024324+tapok39@users.noreply.github.com>
2022-08-15 16:28:48 +0300

    Add files via upload

---
+++ keygenerator.py
@@ -0,0 +1,24 @@
+from random import randint as r
+from os import system as s
+
+chars = ('qwertyuiop[]asdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890//!@#$%^&*())_+')
+
+def create():
+    n = int(input('Введите количество символов пароля: '))
+    password = ''
+    for i in range(n):
+        password = f'{password}{chars[r(0, len(chars)-1)]}'
+    print(password)
+prog = True
+while prog:
+    create()
+    k = int(input('Продолжить? (1/0): '))
+    if k == 1:
+        prog = True
+    elif k == 0:
+        prog = False
+    s('cls')
+
+
+
+
